44224afc872b3e85475c7351c21a25aeeeca6500
long bin <david.lb90@gmail.com>
2023-10-08 13:39:44 +0200

    ffrgehre

--- netlify-functions/accept.js
+++ netlify-functions/accept.js
@@ -1,29 +1,39 @@
-const emailjs = require("emailjs-com/nodejs"); // Assurez-vous d'importer la version correcte
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(
+  "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"
+);
 
 exports.handler = async (event, context) => {
-  emailjs.init("YOUR_USER_ID"); // Remplacez par votre userID
+  if (event.httpMethod !== "GET") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
 
-  const data = {
-    service_id: "service_tm1wxto",
-    template_id: "template_ua6d1za",
-    user_id: "I5f0O3BoNI4d1FJPP",
-    template_params: {
-      from_name: "IL GIRASOLE STRASBOURG",
-      to_email: "david.lb90@gmail.com",
-      message_html: "Test message",
-    },
+  const { email } = event.queryStringParameters;
+
+  const msg = {
+    to: email,
+    from: "david.lb90@gmail.com", // Votre email d'expéditeur
+    subject: "Confirmation de réservation",
+    text: "Votre réservation a été acceptée!",
+    html: "<strong>Votre réservation a été acceptée!</strong>",
   };
 
   try {
-    const response = await emailjs.send(data);
+    await sgMail.send(msg);
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Email sent successfully!" }),
+      body: "Email envoyé avec succès",
     };
   } catch (error) {
+    console.error(error);
+
+    if (error.response) {
+      console.error(error.response.body);
+    }
+
     return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
+      statusCode: error.statusCode || 500,
+      body: "Erreur lors de l'envoi de l'email",
     };
   }
 };
