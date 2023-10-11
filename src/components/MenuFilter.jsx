78f9e4ad33adb9e7ea30414acce3acded5af078d
long bin <david.lb90@gmail.com>
2023-10-11 13:04:30 +0200

    dfgegefg

--- src/components/MenuFilter.jsx
+++ src/components/MenuFilter.jsx
@@ -4,10 +4,10 @@ const MenuFilter = ({ name, onClick }) => {
   return (
     <>
       <button
-        className=" xl:text-xl lg:text-xl md:text-xl sm:text-xl text-sm  "
+        className=" xl:text-xl lg:text-xl md:text-xl sm:text-xl text-xs py-[20px] text-black hover:text-my-gold  "
         onClick={onClick}
       >
-        {name}
+        {name.toUpperCase()}
       </button>
     </>
   );
--- src/page/Menu.jsx
+++ src/page/Menu.jsx
@@ -56,7 +56,7 @@ const Menu = () => {
         }}
       >
         <div className="w-full  relative flex justify-between items-start  ">
-          <div className="sticky top-0 w-1/6 flex flex-col justify-evenly items-start p-4 h-screen ">
+          <div className="sticky top-0 w-1/6 flex flex-col justify-start items-start p-4 h-screen ">
             <MenuFilter
               name={"Antipasti"}
               onClick={() => handleCategoryChange("antipasti")}
