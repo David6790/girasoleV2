e3b886a087e909fe8d6eeecf4f22985e295bbc5a
long bin <david.lb90@gmail.com>
2023-10-12 20:24:07 +0200

    gfzgsgrre

--- src/components/ButtonResa.jsx
+++ src/components/ButtonResa.jsx
@@ -14,7 +14,7 @@ const ButtonResa = () => {
     <>
       <ModalReservation isOpen={isModalOpen} onClose={closeModale} />
       <button
-        className="px-5 py-2 border-solid border-black border-[1px] mt-5 mb-5 text-xl rounded-md shadow-2xl transform transition-transform duration-200 hover:-translate-y-1"
+        className="px-5 py-2 border-solid border-black border-[1px] mt-5 mb-5 xl:text-xl lg:text-xl md:text-xl sm:text-lg text-base rounded-md shadow-2xl transform transition-transform duration-200 hover:-translate-y-1"
         onClick={handleClick}
       >
         Réserver en ligne
