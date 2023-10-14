7022053b68ba92a4a7620c733905a873e0a239f9
long bin <david.lb90@gmail.com>
2023-10-14 17:15:43 +0200

    changement form resa

--- src/components/modals/ModalReservation.jsx
+++ src/components/modals/ModalReservation.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
-import DatePicker from "react-date-picker";
-import "react-date-picker/dist/DatePicker.css";
-import "react-calendar/dist/Calendar.css";
+import Datetime from "react-datetime";
+import "react-datetime/css/react-datetime.css";
+import "moment/locale/fr"; // Importe le locale français
+
+import moment from "moment";
+
 import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 import MessageModal from "./MessageModal";
@@ -10,50 +13,63 @@ import MessageModal from "./MessageModal";
 Modal.setAppElement("#root");
 
 const ModalReservation = ({ isOpen, onClose }) => {
-  const [resDate, setResDate] = useState(new Date());
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [numberOfGuest, setNumberOfGuest] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [time, setTime] = useState("12:00");
+  const [dateTime, setDateTime] = useState(
+    moment().set({ hour: 12, minute: 0 })
+  );
+
+  moment.locale("fr");
+
   const [tel, setTel] = useState("");
   const [messageModalOpen, setMessageModalOpen] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
 
-  const handleTimeChange = (e) => {
-    setTime(e.target.value);
+  const handleChangeDateTime = (value) => {
+    if (value) {
+      setDateTime(value);
+    } else {
+      setDateTime(moment().set({ hour: 12, minute: 0 }));
+    }
   };
-
   const sendEmail = (e) => {
+    const timeValue = dateTime.format("HH:mm");
+    const validDateTime = moment.isMoment(dateTime)
+      ? dateTime
+      : moment(dateTime);
     const data = {
-      date: resDate.toLocaleDateString(),
+      resDate: validDateTime.format("DD-MM-YY"),
+      restime: validDateTime.format("HH:mm"),
       name: name,
       number: numberOfGuest,
       email: email,
       message: message,
       phone: tel,
     };
+
     e.preventDefault();
     setIsLoading(true);
-    if (time > "13:45" && time <= "19:00") {
+    if (timeValue > "13:45" && timeValue <= "19:00") {
       setModalMessage(
         "Nous sommes en pause de 14h00 à 19h00. Veuillez choisir un autre créneau."
       );
       setMessageModalOpen(true);
       setIsLoading(false);
-      setTime("");
-    } else if (time > "21:45") {
+      setDateTime("");
+    } else if (timeValue > "21:45") {
       setModalMessage("Nous ne prenons plus de réservation après 21h45");
       setMessageModalOpen(true);
-      setTime("");
+      setDateTime("");
       setIsLoading(false);
-    } else if (time < "12:00") {
+    } else if (timeValue < "12:00") {
       setModalMessage(
         "Nous ouvrons nos portes à partir de midi. Veuillez choisir un autre créneau"
       );
       setMessageModalOpen(true);
-      setTime("");
+      setDateTime("");
       setIsLoading(false);
     } else {
       emailjs
@@ -64,9 +80,9 @@ const ModalReservation = ({ isOpen, onClose }) => {
             setEmail("");
             setMessage("");
             setName("");
-            setResDate("");
+            setDateTime("");
             setNumberOfGuest("");
-            setTime("");
+
             setTel("");
             setIsLoading(false);
             setModalMessage(
@@ -87,7 +103,7 @@ const ModalReservation = ({ isOpen, onClose }) => {
       onRequestClose={onClose}
       contentLabel="Réservation en ligne"
       className="w-full h-full bg-opacity-60 bg-my-gold backdrop-blur-md rounded-2xl flex flex-col justify-around items-center "
-      overlayClassName="fixed top-0 left-0  h-full w-screen xl:px-40 lg:px-40 md:px-20 sm:px-20 px-5 xl:py-20 lg:py-40   md:py-20 sm:py-20 py-16 z-40 text-left "
+      overlayClassName="fixed top-0 left-0  h-full w-screen xl:px-40 lg:px-40 md:px-20 sm:px-20 px-5 xl:py-20 lg:py-40   md:py-20 sm:py-20 py-5 z-40 text-left bg-black bg-opacity-50 "
     >
       <MessageModal
         isOpen={messageModalOpen}
@@ -95,7 +111,7 @@ const ModalReservation = ({ isOpen, onClose }) => {
         onClose={() => setMessageModalOpen(false)}
       />
       <motion.div
-        className="w-full h-full  rounded-2xl flex flex-col justify-around "
+        className="w-full h-full  rounded-2xl flex flex-col justify-around px-5   "
         initial={{
           opacity: 0,
         }}
@@ -116,12 +132,13 @@ const ModalReservation = ({ isOpen, onClose }) => {
           },
         }}
       >
-        <div className=" w-full flex flex-row justify-between items-center git xl:px-10 lg:px-10 md:px-10 sm:px-10 px-2  text-white">
-          <h1 className=" xl:text-3xl lg:text-3xl md:text-3xl sm:text-3xl text-xl font-title-font text-white font-bold ">
+        <div className=" w-full flex flex-row justify-between items-center git xl:px-10 lg:px-10 md:px-10 sm:px-5 px-2  text-black h-[70px] mb-10  ">
+          <img src="./img/logoWhite.png" alt="" className=" h-full" />
+          <h1 className=" xl:text-3xl lg:text-3xl md:text-2xl  xl:block lg:block md:block sm:hidden hidden  font-title-font  font-bold text-white ">
             Réservation en ligne
           </h1>
           <button
-            className=" px-4 py-2 bg-transparent text-white "
+            className=" px-4 py-2 bg-transparent text-white  "
             onClick={onClose}
           >
             Fermer
@@ -130,81 +147,71 @@ const ModalReservation = ({ isOpen, onClose }) => {
 
         <form
           onSubmit={sendEmail}
-          className=" flex flex-col justify-between xl:px-10 lg:px-10 md:px-10 sm:px-10 px-2 w-full h-[80%] overflow-scroll text-white "
+          className=" flex flex-col justify-between xl:px-10 lg:px-10 md:px-10 sm:px-2 px-2 w-full h-[100%] overflow-scroll text-white font-bold "
         >
           <div className=" flex xl:flex-row lg:flex-row md:flex-row sm:flex-row flex-col justify-between items-center h-[80%] ">
             <div className="flex  flex-col xl:w-[48%] lg:w-[48%] md:w-[48%] sm:w-[48%] w-full h-full  ">
-              <label>Nom de la réservation</label>
+              <label className="text-white">Nom de la réservation</label>
               <input
-                placeholder="Votre Nom"
                 value={name}
                 type="text"
                 name="name"
                 onChange={(e) => setName(e.target.value)}
-                className="border-[1px] border-black h-[40px] mb-2 text-black focus:outline-none"
+                className="  h-[40px] bg-transparent focus:outline-none border-b-[1px] px-2 mb-5 "
                 required
               />
-              <label>Date de la réservation</label>
-              <DatePicker
-                selected={resDate}
-                value={resDate}
-                onChange={(date) => setResDate(date)}
-                className=" bg-white  border-black h-[40px]  mb-2 w-full flex flex-row text-black  focus:outline-none"
-                required
-              />
-              <label>Heure d'arrivé</label>
+              <label className="text-white ">Nombre de personne</label>
               <input
-                type="time"
-                name="resTime"
-                className=" bg-white  border-black border-[1px] h-[40px]  mb-2 text-black focus:outline-none "
-                value={time}
-                onChange={handleTimeChange}
-                required
-              />
-
-              <label className=" mb-0">Nombre de personne</label>
-              <input
-                placeholder="Nombre de personnes"
                 value={numberOfGuest}
                 type="number"
                 name="number"
                 onChange={(e) => setNumberOfGuest(e.target.value)}
-                className="border-[1px] border-black h-[40px]  mb-2 text-black  focus:outline-none"
+                className=" h-[40px]   focus:outline-none  bg-transparent border-b-[1px] px-2 mb-5"
                 required
               />
-              <label>Email</label>
+              <label className="text-white">Date et heure</label>
+              <Datetime
+                locale="fr"
+                value={dateTime}
+                onChange={handleChangeDateTime}
+                dateFormat="YYYY-MM-DD"
+                timeFormat="HH:mm"
+                inputProps={{ placeholder: "Sélectionnez la date et l'heure" }}
+                timeConstraints={{ minutes: { step: 15 } }}
+                required
+                className="  h-[40px] flex flex-row justify-start items-center bg-transparent border-b-[1px] px-2 mb-5 "
+              />
+
+              <label className="text-white mb-1">Email</label>
               <input
-                placeholder="Votre E-mail"
                 value={email}
                 type="email"
                 name="email"
                 onChange={(e) => setEmail(e.target.value)}
-                className="border-[1px] border-black h-[40px]  mb-2 text-black focus:outline-none "
+                className="h-[40px]  mb-5  focus:outline-none bg-transparent border-b-[1px] px-2"
                 required
               />
-              <label>Télephone</label>
+              <label className="text-white ">Télephone</label>
               <input
-                placeholder="Un numéro pour vous joindre"
                 value={tel}
                 type="tel"
                 name="phone"
                 onChange={(e) => setTel(e.target.value)}
-                className="border-[1px] border-black h-[40px]  mb-2 text-black focus:outline-none"
+                className="  h-[40px]  mb-5  focus:outline-none bg-transparent border-b-[1px] px-2"
                 required
               />
             </div>
-            <div className=" flex flex-col xl:w-[48%] lg:w-[48%] md:w-[48%] sm:w-[48%] w-full h-full ">
-              <label className="text-base">Commentaires</label>
+            <div className=" flex flex-col xl:w-[48%] lg:w-[48%] md:w-[48%] sm:w-[48%] w-full h-full  ">
+              <label className="text-white">Commentaires</label>
               <textarea
                 name="message"
-                placeholder="Une demande spéciale ? Laissez-nous un commentaire, nous ferrons au mieux pour vous satisfaire"
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
-                className=" h-[70%] focus:outline-none  resize-none border-[1px] border-black  p-2 text-black "
+                className=" h-[70%] focus:outline-none  resize-none   p-2  mb-5 bg-transparent border-b-[1px] px-2 "
               />
               <button
                 type="submit"
-                className=" border-2 border-black w-full m-auto h-[50px] bg-white hover:bg-black text-black hover:text-white mt-5 "
+                className="  w-full m-auto h-[40px]   mt-3 text-white text-xl font-title-font border-[1px] mb-10 "
               >
                 {isLoading ? "En cours..." : "Envoyer"}
               </button>
