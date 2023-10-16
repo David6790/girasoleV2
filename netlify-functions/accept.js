edbbf05685eb86b7e5ce3007e2119d4ec3245223
long bin <david.lb90@gmail.com>
2023-10-16 16:38:12 +0200

    ajout il girasole sender id

--- netlify-functions/accept.js
+++ netlify-functions/accept.js
@@ -28,7 +28,7 @@ exports.handler = async (event, context) => {
     if (phone) {
       await twilioClient.messages.create({
         body: `${greeting} ${name}, votre réservation au Il Girasole au nom de ${name}, le ${resDate} à ${resTime} pour ${number} personnes a bien été notée et nous vous en remercions. En cas d'empêchement, n'oubliez pas de nous appeler au plus vite, au 03 88 37 16 76 ou par sms au 06 26 19 10 28 (en indiquant votre nom).`,
-        from: "+12295979254",
+        from: "IlGirasole",
         to: `+${phone}`,
       });
     }
