d2c7c43dcf4f8ec4c0635b29bc8546511f9ec55f
long bin <david.lb90@gmail.com>
2023-11-06 12:13:18 +0100

    ok

--- netlify-functions/reject.js
+++ netlify-functions/reject.js
@@ -27,7 +27,7 @@ exports.handler = async (event, context) => {
       await twilioClient.messages.create({
         body: `${greeting} ${name}, merci pour votre réservation mais malheureusement le restaurant est au complet ce soir. Si vous le souhaitez nous pouvons vous mettre sur une liste d'attente et vous contacter dès qu'une table se libère.
         À bientôt -IL GIRASOLE-`,
-        from: "+12295979254",
+        from: "IlGirasole",
         to: `+${phone}`,
       });
     }
