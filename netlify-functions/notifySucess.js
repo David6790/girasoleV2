b86a07a3da21a90a77740defee0b920cfe062417
long bin <david.lb90@gmail.com>
2023-11-19 17:25:47 +0100

    checkout

--- netlify-functions/notifySucess.js
+++ netlify-functions/notifySucess.js
@@ -20,11 +20,11 @@ exports.handler = async (lambdaEvent, context) => {
       webhookSecret
     );
 
-    if (stripeEvent.type === "payment_intent.succeeded") {
-      const paymentIntent = stripeEvent.data.object;
-      const reservationId = paymentIntent.metadata.reservationId;
-      console.log("Payment Intent:", paymentIntent);
-      console.log("Metadata:", paymentIntent.metadata);
+    if (stripeEvent.type === "checkout.session.completed") {
+      const checkoutSession = stripeEvent.data.object;
+      const metadata = checkoutSession.metadata;
+      const reservationId = metadata.reservationId;
+
       // Mise à jour de SheetDB
       // await axios.patch(
       //   `https://sheetdb.io/api/v1/97lppk2d46b57/ID/${reservationId}`,
