69945f4b8fe6ae2d78ded54af4fc8ca9b49a1bfc
long bin <david.lb90@gmail.com>
2024-02-19 14:22:16 +0100

    djfjfjddjh

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -13,6 +13,8 @@ const Dashboard = () => {
 
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-white">
+      <h1 className=" text-3xl">Dashboard IL GIRASOLE</h1>
+      <h2 className="text-xl mb-10">By David Long Bin 😎</h2>
       <ModalStaff isOpen={isModalOpen} onClose={closeModale} />
       <button
         className="px-5 py-2 border-solid border-black border-[1px] mt-5 mb-5 xl:text-xl lg:text-xl md:text-xl sm:text-lg text-base rounded-md shadow-2xl transform transition-transform duration-200 hover:-translate-y-1"
